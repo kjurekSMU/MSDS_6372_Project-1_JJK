175d83301847a3c6ff9721632bb6ff5e6113964f
pankaj <pankaj.tiwari2@gmail.com>
2019-06-07 05:21:30 +0800

    adding R code for model and outlier

--- Analysis/eda.R
+++ Analysis/eda.R
@@ -1,16 +1,22 @@
 library(tidyverse)
+library(stats)
+library(broom)
+
+sas_directory = '/Users/pankaj/dev/sas/SASUniversityEdition/myfolders/stats2_hw'
 
 price_modeling_data = read_csv('Analysis/Data/modelingData.csv' ,
                          col_types = cols(.default = 'i'  ))
 
 
-p = ggplot(data = price_modeling_data, aes( x= life_sq,y = price_doc))
+p = ggplot(data = price_modeling_data )
 
-p+geom_point(mapping = aes( x= life_sq)) +
-  stat_smooth(method = "lm", col = "red")
+p+geom_point(mapping = aes( x= full_sq)) 
 
+view(price_modeling_data$full_sq)
 
+p+geom_point(mapping = aes( x= full_all)) 
 
+p+geom_histogram(mapping = aes( x= full_all)) 
 
 p+geom_point(mapping = aes( x = num_room))
 
@@ -22,9 +28,55 @@ dev.new()
 p+geom_bar(mapping = aes( x = radiation_raion), stat = "identity") 
 
 #coord_flip()
-
-
 p+geom_point(mapping = aes( x= kitch_sq))
+summarise(price_modeling_data$radiation_raion)
+
+### count vars that are all none or  no none
+na_count <-sapply(price_modeling_data, function(y) sum(length(which(is.na(y)))))
+na_count_frame= stack(na_count)
+all_na= filter(na_count_frame, na_count==25471)
+no_na = filter(na_count_frame, na_count==0)
+view(no_na)
+
+hist(na_count_frame$values)
+
+pr_model = lm(price_doc ~ full_sq+work_all + as.factor(sub_area), price_modeling_data)
+cookd=cooks.distance(pr_model)
+summary(pr_model)
+
+df <- augment(pr_model)
+ggplot(df, aes(x = .fitted, y = .resid)) + geom_point()
+
+
+# ignore with cookd greater than 4 to filter outliers
+
+cookd=cooks.distance(pr_model)
+
+reduced_model_data = 
+  select( price_modeling_data , c(no_na$ind))  %>% 
+  filter(row_number() %in% names(Filter(function(x){x<4}, cookd)), full_sq <200  ) %>% 
+  # additional filter based graph
+  filter(id!=2121) %>% filter(full_sq>=5)
+
+## fit smaller model
+
+pr_model = lm(price_doc ~ full_sq +work_all+ as.factor(ID_metro) + as.factor(children_school)+
+                as.factor(healthcare_centers_raion),  
+              reduced_model_data)
+summary(pr_model)
 
 
 
+ggplot(data= reduced_model_data, aes( x=shopping_centers_raion, y = price_doc ))+
+  geom_point()+
+  geom_smooth(method= "lm", color = "red")
+ 
+
+
+cookd[2321]
+
+Filter(function(x){x>2}, cookd)[2321]
+
+plot(cookd)
+
+summary(pr_model)
